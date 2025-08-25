b561341c82eda17b8bc2e62b3dfeb62691d9171b
Syed Ali Shah <sa.syedali2000@gmail.com>
2025-08-25 21:57:30 +0500

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-  darkMode: "class", // <-- YEH LINE ADD KARNI HAI
+  darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
